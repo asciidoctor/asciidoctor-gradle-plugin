d064457f86d17d6878b6c478d1329bc480100e57
aalmiray <aalmiray@gmail.com>
2013-02-04 14:43:43 +0100

    Add Spock specs

--- src/main/groovy/org/asciidoctor/gradle/AsciidoctorTask.groovy
+++ src/main/groovy/org/asciidoctor/gradle/AsciidoctorTask.groovy
@@ -20,11 +20,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
-
-import javax.script.Bindings
-import javax.script.ScriptEngine
-import javax.script.ScriptEngineManager
-import javax.script.SimpleBindings
+import org.gradle.tooling.BuildException
 
 /**
  * @author Noam Tenne
@@ -36,27 +32,24 @@ class AsciidoctorTask extends DefaultTask {
     @Input String backend
 
     AsciidoctorTask() {
-        sourceDir = project.file("src/asciidoc")
+        sourceDir = project.file('src/asciidoc')
         outputDir = new File("${project.buildDir}/asciidoc")
         backend = 'html5'
     }
 
     @TaskAction
     void gititdone() {
-        final ScriptEngineManager engineManager = new ScriptEngineManager();
-        final ScriptEngine rubyEngine = engineManager.getEngineByName("jruby");
-        final Bindings bindings = new SimpleBindings();
-
-        bindings.put("srcDir", sourceDir.getAbsolutePath());
-        bindings.put("outputDir", outputDir.getAbsolutePath());
-        bindings.put("backend", backend);
+        AsciidoctorWorker worker = new AsciidoctorWorker(
+            sourceDir: sourceDir,
+            outputDir: outputDir,
+            backend: backend
+        )
 
         try {
-            final InputStream script = AsciidoctorTask.class.getClassLoader().getResourceAsStream("execute_asciidoctor.rb");
-            final InputStreamReader streamReader = new InputStreamReader(script);
-            rubyEngine.eval(streamReader, bindings);
-        } catch (ScriptException e) {
-            project.getLogger().error("Error running ruby script", e);
+            worker.execute()
+        } catch (Exception e) {
+            project.getLogger().error('Error running ruby script', e)
+            throw new BuildException('Error running ruby script', e)
         }
     }
 }
