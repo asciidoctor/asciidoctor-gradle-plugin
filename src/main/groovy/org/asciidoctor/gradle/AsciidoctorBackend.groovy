c1fc9ff496cb1d64a4c6afb6d3f08008d3291ba3
Benjamin Muschko <benjamin.muschko@gmail.com>
2013-02-21 18:36:34 -0500

    Added more tests. Fixed up the documentation. Minor refactorings.

---
+++ src/main/groovy/org/asciidoctor/gradle/AsciidoctorBackend.groovy
@@ -0,0 +1,35 @@
+package org.asciidoctor.gradle
+
+/**
+ * Supported backends.
+ *
+ * @author Benjamin Muschko
+ */
+enum AsciidoctorBackend {
+    HTML5('html5')
+
+    private final static Map<String, AsciidoctorBackend> ALL_BACKENDS
+    private final String id
+
+    static {
+        ALL_BACKENDS = new HashMap<String, AsciidoctorBackend>()
+
+        values().each {
+            ALL_BACKENDS[it.id] = it
+        }
+
+        ALL_BACKENDS.asImmutable()
+    }
+
+    private AsciidoctorBackend(String id) {
+        this.id = id
+    }
+
+    String getId() {
+        id
+    }
+
+    static boolean isSupported(String name) {
+        ALL_BACKENDS.containsKey(name)
+    }
+}
