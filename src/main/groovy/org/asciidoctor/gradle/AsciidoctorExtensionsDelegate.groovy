6a9b5f6fcbadbf0013e2deb48de703317e118235
Robert.Panzer <robert.panzer@me.com>
2014-08-19 14:09:13 +0200

    Added support for pre- and postprocessor extensions
    Added support for IncludeProcessors
    
    Added support for block and inline macro processors

--- src/main/groovy/org/asciidoctor/gradle/AsciidoctorExtensionsDelegate.groovy
+++ src/main/groovy/org/asciidoctor/gradle/AsciidoctorExtensionsDelegate.groovy
@@ -6,6 +6,11 @@ import java.util.concurrent.atomic.AtomicInteger
 
 public class AsciidoctorExtensionsDelegate {
 
+	private class ExtensionHolder {
+		Map options
+		Closure closure
+	}
+
 	private AsciidoctorTask task
 
 	private List<ExtensionHolder> blockProcessors = []
@@ -36,9 +41,9 @@ public class AsciidoctorExtensionsDelegate {
 
 	void registerExtensions(Object javaExtensionRegistry, GroovyClassLoader groovyClassLoader) {
 		blockProcessors.each {
-			javaExtensionRegistry.block(createBlockProcessor(it[0], it[1], groovyClassLoader))
+			javaExtensionRegistry.block(createBlockProcessor(it.options, it.closure, groovyClassLoader))
 		}
-        postProcessors.each {
+		postProcessors.each {
 			javaExtensionRegistry.postprocessor(createPostProcessor(it.options, it.closure, groovyClassLoader))
 		}
 		preProcessors.each {
@@ -95,14 +100,186 @@ class $simpleClassName extends org.asciidoctor.extension.BlockProcessor {
 			//TODO: What are sensible defaults?
 			options["contexts"] = [":open", ":paragraph"]
 		}
-		blockProcessors << [options, cl]
+		blockProcessors << new ExtensionHolder(options: options, closure: cl)
 	}
 
 	void block(String blockName, Closure cl) {
 		block(["name": blockName], cl)
 	}
 
-	// Everything for other processors
+	// Everything for PostProcessors
+
+	Object createPostProcessor(Map options, Closure cl, GroovyClassLoader groovyClassLoader) {
+
+		def simpleClassName = "Extension_" + extensionClassCounter.getAndIncrement()
+		def packageName = "org.asciidoctor.gradle.extensions"
+		def classContent = """package $packageName
+import org.asciidoctor.ast.Document;
+import org.asciidoctor.ast.DocumentRuby;
+class $simpleClassName extends org.asciidoctor.extension.Postprocessor {
+
+		private Closure cl
+
+		public $simpleClassName(Map options, Closure cl) {
+			super(options)
+			this.cl = cl
+		}
+
+		public String process(Document document, String output) {
+			cl.call(document, output)
+		}
+}"""
+
+		Class clazz = groovyClassLoader.parseClass(classContent)
+		Object processor = clazz.newInstance(options, cl)
+		cl.delegate = processor
+		return processor
+	}
+
+	void postprocessor(Map options=[:], Closure cl) {
+		postProcessors << new ExtensionHolder(options: options, closure: cl)
+	}
+
+	// Everything for PreProcessors
+
+	Object createPreProcessor(Map options, Closure cl, GroovyClassLoader groovyClassLoader) {
+
+		def simpleClassName = "Extension_" + extensionClassCounter.getAndIncrement()
+		def packageName = "org.asciidoctor.gradle.extensions"
+		def classContent = """package $packageName
+import org.asciidoctor.ast.Document;
+import org.asciidoctor.extension.PreprocessorReader;
+class $simpleClassName extends org.asciidoctor.extension.Preprocessor {
+
+		private Closure cl
+
+		public $simpleClassName(Map options, Closure cl) {
+			super(options)
+			this.cl = cl
+		}
+
+		public PreprocessorReader process(Document document, PreprocessorReader reader) {
+			cl.call(document, reader)
+		}
+}"""
+
+		Class clazz = groovyClassLoader.parseClass(classContent)
+		Object processor = clazz.newInstance(options, cl)
+		cl.delegate = processor
+		return processor
+	}
+
+	void preprocessor(Map options=[:], Closure cl) {
+		preProcessors << new ExtensionHolder(options: options, closure: cl)
+	}
+
+	// Everything for IncludeProcessors
+
+	Object createIncludeProcessor(Map options, Closure cl, GroovyClassLoader groovyClassLoader) {
+
+		def simpleClassName = "Extension_" + extensionClassCounter.getAndIncrement()
+		def packageName = "org.asciidoctor.gradle.extensions"
+		def classContent = """package $packageName
+import org.asciidoctor.ast.Document;
+import org.asciidoctor.extension.PreprocessorReader;
+class $simpleClassName extends org.asciidoctor.extension.IncludeProcessor {
+
+		private Closure filter
+		private Closure cl
+
+		public $simpleClassName(Map options, Closure filter, Closure cl) {
+			super(options)
+			this.filter = filter
+			this.cl = cl
+		}
+		public boolean handles(String target) {
+			filter.call(target)
+		}
+		public void process(PreprocessorReader reader, String target, Map<String, Object> attributes) {
+			cl.call(reader, target, attributes)
+		}
+}"""
+
+		Class clazz = groovyClassLoader.parseClass(classContent)
+		Closure filter = options["filter"]
+		options.remove("filter")
+		Object processor = clazz.newInstance(options, filter, cl)
+		cl.delegate = processor
+		return processor
+	}
+
+	void includeprocessor(Map options=[:], Closure cl) {
+		includeProcessors << new ExtensionHolder(options: options, closure: cl)
+	}
+
+
+	// Everything for MacroProcessors
+
+	Object createBlockMacroProcessor(Map options, Closure cl, GroovyClassLoader groovyClassLoader) {
+
+		def simpleClassName = "Extension_" + extensionClassCounter.getAndIncrement()
+		def packageName = "org.asciidoctor.gradle.extensions"
+		def classContent = """package $packageName
+import org.asciidoctor.ast.AbstractBlock;
+class $simpleClassName extends org.asciidoctor.extension.BlockMacroProcessor {
+
+		private Closure cl
+
+		public $simpleClassName(String name, Map options, Closure cl) {
+			super(name, options)
+			this.cl = cl
+		}
+		public Object process(AbstractBlock parent, String target, Map<String, Object> attributes) {
+			cl.call(parent, target, attributes)
+		}
+}"""
+
+		Class clazz = groovyClassLoader.parseClass(classContent)
+		Object processor = clazz.newInstance(options["name"], options, cl)
+		cl.delegate = processor
+		return processor
+	}
+
+	void blockMacro(Map options=[:], Closure cl) {
+		blockMacroProcessors << new ExtensionHolder(options: options, closure: cl)
+	}
+
+	// Everything for InlineMacroProcessors (?? Configure in the same way as a MacroProcessor?)
+
+	Object createInlineMacroProcessor(Map options, Closure cl, GroovyClassLoader groovyClassLoader) {
+
+		def simpleClassName = "Extension_" + extensionClassCounter.getAndIncrement()
+		def packageName = "org.asciidoctor.gradle.extensions"
+		def classContent = """package $packageName
+import org.asciidoctor.ast.AbstractBlock;
+class $simpleClassName extends org.asciidoctor.extension.InlineMacroProcessor {
+
+		private Closure cl
+
+		public $simpleClassName(String name, Map options, Closure cl) {
+			super(name, options)
+			this.cl = cl
+		}
+		public Object process(AbstractBlock parent, String target, Map<String, Object> attributes) {
+			cl.call(parent, target, attributes)
+		}
+}"""
+
+		Class clazz = groovyClassLoader.parseClass(classContent)
+		Object processor = clazz.newInstance(options["name"], options, cl)
+		cl.delegate = processor
+		return processor
+	}
+
+	void inlineMacro(Map options=[:], Closure cl) {
+		inlineMacroProcessors << new ExtensionHolder(options: options, closure: cl)
+	}
+
+	void inlineMacro(String macroName, Closure cl) {
+		inlineMacroProcessors << new ExtensionHolder(["name": macroName], closure: cl)
+	}
+
+	// Everything for TreeProcessors
 
 	Object createTreeProcessor(Map options, Closure cl, GroovyClassLoader groovyClassLoader) {
 
--- src/test/groovy/org/asciidoctor/gradle/AsciidoctorTaskInlineExtensionsSpec.groovy
+++ src/test/groovy/org/asciidoctor/gradle/AsciidoctorTaskInlineExtensionsSpec.groovy
@@ -21,6 +21,9 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.internal.file.collections.SimpleFileCollection
 import org.gradle.testfixtures.ProjectBuilder
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
 import spock.lang.Specification
 
 /**
@@ -29,22 +32,22 @@ import spock.lang.Specification
  * @author Robert Panzer
  */
 class AsciidoctorTaskInlineExtensionsSpec extends Specification {
-	private static final String ASCIIDOCTOR = 'asciidoctor'
-	private static final String ASCIIDOC_RESOURCES_DIR = 'build/resources/test/src/asciidocextensions'
-	private static final String ASCIIDOC_BUILD_DIR = 'build/asciidocextensions'
-	private static final String ASCIIDOC_INLINE_EXTENSIONS_FILE = 'inlineextensions.asciidoc'
-	private static final String ASCIIDOC_INLINE_EXTENSIONS_RESULT_FILE = 'inlineextensions.html'
-	private static final DOCINFO_FILE_PATTERN = ~/^(.+\-)?docinfo(-footer)?\.[^.]+$/
+    private static final String ASCIIDOCTOR = 'asciidoctor'
+    private static final String ASCIIDOC_RESOURCES_DIR = 'build/resources/test/src/asciidocextensions'
+    private static final String ASCIIDOC_BUILD_DIR = 'build/asciidocextensions'
+    private static final String ASCIIDOC_INLINE_EXTENSIONS_FILE = 'inlineextensions.asciidoc'
+    private static final String ASCIIDOC_INLINE_EXTENSIONS_RESULT_FILE = 'inlineextensions.html'
+    private static final DOCINFO_FILE_PATTERN = ~/^(.+\-)?docinfo(-footer)?\.[^.]+$/
 
-	Project project
-	File testRootDir
+    Project project
+    File testRootDir
 
-	def setup() {
-		project = ProjectBuilder.builder().withName('test').build()
-		testRootDir = new File('.')
-	}
+    def setup() {
+        project = ProjectBuilder.builder().withName('test').build()
+        testRootDir = new File('.')
+    }
 
-
+	
 	def "Should apply inline BlockProcessor"() {
 		given:
 		Task task = project.tasks.create(name: ASCIIDOCTOR, type: AsciidoctorTask) {
@@ -267,6 +270,133 @@ class AsciidoctorTaskInlineExtensionsSpec extends Specification {
 		then:
 			resultFile.exists()
 			resultFile.getText().contains('<span class="command">')
+	}
+
+	def "Should apply inline Postprocessor"() {
+		given:
+			String copyright = "Copyright Acme, Inc." + System.currentTimeMillis()
+			Task task = project.tasks.create(name: ASCIIDOCTOR, type: AsciidoctorTask) {
+				sourceDir = new File(testRootDir, ASCIIDOC_RESOURCES_DIR)
+				outputDir = new File(testRootDir, ASCIIDOC_BUILD_DIR)
+				sourceDocumentNames = project.files(
+					"${testRootDir}/${ASCIIDOC_INLINE_EXTENSIONS_FILE}")
+				extensions {
+					postprocessor {
+						document, output ->
+							if(document.basebackend("html")) {
+								org.jsoup.nodes.Document doc = Jsoup.parse(output, "UTF-8")
+
+								Element contentElement = doc.getElementById("footer-text")
+								contentElement.append(copyright)
+            
+								output = doc.html()
+							} else {
+								throw new IllegalArgumentException("Expected html!")
+							} 
+					}
+				}
+			}
+			File resultFile = new File(testRootDir, ASCIIDOC_BUILD_DIR + File.separator + ASCIIDOC_INLINE_EXTENSIONS_RESULT_FILE)
+		when:
+			task.processAsciidocSources()
+		then:
+			resultFile.exists()
+			resultFile.getText().contains(copyright)
+			resultFile.getText().contains("Inline Extension Test document")
+			
+	}
+
+	def "Should apply inline Preprocessor"() {
+		given:
+			Task task = project.tasks.create(name: ASCIIDOCTOR, type: AsciidoctorTask) {
+				sourceDir = new File(testRootDir, ASCIIDOC_RESOURCES_DIR)
+				outputDir = new File(testRootDir, ASCIIDOC_BUILD_DIR)
+				sourceDocumentNames = project.files(
+					"${testRootDir}/${ASCIIDOC_INLINE_EXTENSIONS_FILE}")
+				extensions {
+					preprocessor {
+						document, reader ->
+							reader.advance()
+							reader
+					}
+				}
+			}
+			File resultFile = new File(testRootDir, ASCIIDOC_BUILD_DIR + File.separator + ASCIIDOC_INLINE_EXTENSIONS_RESULT_FILE)
+		when:
+			task.processAsciidocSources()
+		then:
+			resultFile.exists()
+			!resultFile.getText().contains("Inline Extension Test document")
+	}
+
+	def "Should apply inline Includeprocessor"() {
+		given:
+			String content = "The content of the URL " + System.currentTimeMillis()
+			Task task = project.tasks.create(name: ASCIIDOCTOR, type: AsciidoctorTask) {
+				sourceDir = new File(testRootDir, ASCIIDOC_RESOURCES_DIR)
+				outputDir = new File(testRootDir, ASCIIDOC_BUILD_DIR)
+				sourceDocumentNames = project.files(
+					"${testRootDir}/${ASCIIDOC_INLINE_EXTENSIONS_FILE}")
+				extensions {
+					includeprocessor (filter: {it.startsWith("http")}) {
+						reader, target, attributes ->
+						reader.push_include(content, target, target, 1, attributes);					}
+				}
+			}
+			File resultFile = new File(testRootDir, ASCIIDOC_BUILD_DIR + File.separator + ASCIIDOC_INLINE_EXTENSIONS_RESULT_FILE)
+		when:
+			task.processAsciidocSources()
+		then:
+			resultFile.exists()
+			resultFile.getText().contains(content)
+	}
+
+	def "Should apply inline BlockMacroProcessor"() {
+		given:
+			Task task = project.tasks.create(name: ASCIIDOCTOR, type: AsciidoctorTask) {
+				sourceDir = new File(testRootDir, ASCIIDOC_RESOURCES_DIR)
+				outputDir = new File(testRootDir, ASCIIDOC_BUILD_DIR)
+				sourceDocumentNames = project.files(
+					"${testRootDir}/${ASCIIDOC_INLINE_EXTENSIONS_FILE}")
+				extensions {
+					blockMacro (name: "gist") {
+						parent, target, attributes ->
+							String content = """<div class="content"> 
+<script src="https://gist.github.com/${target}.js"></script> 
+</div>""" 
+	       					createBlock(parent, "pass", [content], attributes, config);
+					}
+				}
+			}
+			File resultFile = new File(testRootDir, ASCIIDOC_BUILD_DIR + File.separator + ASCIIDOC_INLINE_EXTENSIONS_RESULT_FILE)
+		when:
+			task.processAsciidocSources()
+		then:
+			resultFile.exists()
+			resultFile.getText().contains("https://gist.github.com/123456.js")
+	}
+
+	def "Should apply inline InlineMacroProcessor"() {
+		given:
+			Task task = project.tasks.create(name: ASCIIDOCTOR, type: AsciidoctorTask) {
+				sourceDir = new File(testRootDir, ASCIIDOC_RESOURCES_DIR)
+				outputDir = new File(testRootDir, ASCIIDOC_BUILD_DIR)
+				sourceDocumentNames = project.files(
+					"${testRootDir}/${ASCIIDOC_INLINE_EXTENSIONS_FILE}")
+				extensions {
+					inlineMacro (name: "man") {
+						parent, target, attributes ->
+						options=["type": ":link", "target": target + ".html"]
+        				createInline(parent, "anchor", target, attributes, options).render()
+					}
+				}
+			}
+			File resultFile = new File(testRootDir, ASCIIDOC_BUILD_DIR + File.separator + ASCIIDOC_INLINE_EXTENSIONS_RESULT_FILE)
+		when:
+			task.processAsciidocSources()
+		then:
+			resultFile.exists()
+			resultFile.getText().contains('<a href="gittutorial.html">gittutorial</a>')
 	}
 
 }
