cce387f50ce995b4dfa85b72929a6224521db318
Schalk W. Cronjé <ysb33r@gmail.com>
2014-09-10 18:55:46 +0100

    Removed a @IgnoreRest that was accidentaly left behind

--- src/test/groovy/org/asciidoctor/gradle/AsciidoctorTaskSpec.groovy
+++ src/test/groovy/org/asciidoctor/gradle/AsciidoctorTaskSpec.groovy
@@ -22,7 +22,6 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.internal.file.collections.SimpleFileCollection
 import org.gradle.testfixtures.ProjectBuilder
-import spock.lang.IgnoreRest
 import spock.lang.Specification
 
 /**
