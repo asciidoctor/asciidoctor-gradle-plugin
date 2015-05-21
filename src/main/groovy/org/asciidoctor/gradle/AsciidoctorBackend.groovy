bbfb7277dfb614c506abec2ca40c7ebba2fece59
aalmiray <aalmiray@yahoo.com>
2015-05-21 12:14:35 +0200

    Support xhtml & xhtml5 backends. Fixes #156

--- src/main/groovy/org/asciidoctor/gradle/AsciidoctorBackend.groovy
+++ src/main/groovy/org/asciidoctor/gradle/AsciidoctorBackend.groovy
@@ -22,7 +22,15 @@ package org.asciidoctor.gradle
  * @author Dan Allen
  */
 enum AsciidoctorBackend {
-    HTML('html'), DOCBOOK('docbook'), HTML5('html5'), DOCBOOK45('docbook45'), DOCBOOK5('docbook5'), EPUB3('epub3'), PDF('pdf')
+    HTML('html'),
+    DOCBOOK('docbook'),
+    HTML5('html5'),
+    DOCBOOK45('docbook45'),
+    DOCBOOK5('docbook5'),
+    EPUB3('epub3'),
+    PDF('pdf'),
+    XHTML('xhtml'),
+    XHTML5('xhtml5'),
 
     private final static Map<String, AsciidoctorBackend> ALL_BACKENDS
     private final String id
