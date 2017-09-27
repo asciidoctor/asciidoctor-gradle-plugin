44681499b35cb4df93d45927c2aaf6b3e9e313bc
aalmiray <aalmiray@yahoo.com>
2017-09-27 19:05:41 +0200

    Update to 1.5.6

--- src/main/groovy/org/asciidoctor/gradle/AsciidoctorExtension.groovy
+++ src/main/groovy/org/asciidoctor/gradle/AsciidoctorExtension.groovy
@@ -21,7 +21,7 @@ import org.gradle.api.Project
  * @author Andres Almiray
  */
 class AsciidoctorExtension {
-    String version = '1.5.5'
+    String version = '1.5.6'
 
     String groovyDslVersion = '1.0.0.Alpha2'
 
