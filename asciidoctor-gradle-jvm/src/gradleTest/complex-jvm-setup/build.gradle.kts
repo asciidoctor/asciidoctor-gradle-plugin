683161016401f99d58e487963425dd9cb407726e
Schalk W. Cronjé <ysb33r@gmail.com>
2019-03-28 20:20:17 +0100

    Use Asciidoctorj 2.0.0-RC.2 & EPUB 1.5.0-alpha.9
    - Use AsciidoctorJRuby.Factory
    - Use org.asciidoctor.jruby.extension.spi.ExtensionRegistry

--- asciidoctor-gradle-jvm/src/gradleTest/complex-jvm-setup/build.gradle.kts
+++ asciidoctor-gradle-jvm/src/gradleTest/complex-jvm-setup/build.gradle.kts
@@ -15,7 +15,8 @@ repositories {
 }
 
 asciidoctorj {
-    modules.getDiagram().setVersion("1.5.4.1")
+    modules.getDiagram().setVersion("1.5.16")
+    setDiagramVersion("1.5.16")
     logLevel = LogLevel.INFO
 }
 
