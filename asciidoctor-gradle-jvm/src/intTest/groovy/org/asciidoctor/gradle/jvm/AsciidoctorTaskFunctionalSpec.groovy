1f1e4cac31e860bfc0985f2edaba5ee08195a902
Schalk W. Cronjé <ysb33r@gmail.com>
2019-04-11 15:19:51 +0200

    Do not add empty GEM paths for multiple backends (#324)
    When running in JAVA_EXEC mode with three or more backends, but
    no additional GEMs specified, the system failed with
    `ArrayIndexOutOfBoundsException`. The reason was that `::` was
    passed down to AsciidoctorJ as the GEM path. The solution is
    not to add empty GEM paths to the GEM search path.

--- asciidoctor-gradle-jvm/src/intTest/groovy/org/asciidoctor/gradle/jvm/AsciidoctorTaskFunctionalSpec.groovy
+++ asciidoctor-gradle-jvm/src/intTest/groovy/org/asciidoctor/gradle/jvm/AsciidoctorTaskFunctionalSpec.groovy
@@ -174,6 +174,28 @@ class AsciidoctorTaskFunctionalSpec extends FunctionalSpecification {
 
     }
 
+    @Issue('https://github.com/asciidoctor/asciidoctor-gradle-plugin/issues/234')
+    void 'Run conversion with an unknown backend using JAVA_EXEC'() {
+        given:
+        getBuildFile('''
+        asciidoctor {
+            inProcess = JAVA_EXEC
+            outputOptions {
+                backends = ['html5', 'abc', 'xyz']
+            }
+            sourceDir 'src/docs/asciidoc'
+        }
+        ''')
+
+        when:
+        String result = getGradleRunner(DEFAULT_ARGS).buildAndFail().output
+
+        then:
+        result.contains("missing converter for backend 'abc'. Processing aborted")
+        result.contains('org.asciidoctor.internal.AsciidoctorCoreException: org.jruby.exceptions.NotImplementedError')
+        !result.contains('ArrayIndexOutOfBoundsException')
+    }
+
     File getBuildFile(String extraContent) {
         getJvmConvertGroovyBuildFile("""
 asciidoctorj {
