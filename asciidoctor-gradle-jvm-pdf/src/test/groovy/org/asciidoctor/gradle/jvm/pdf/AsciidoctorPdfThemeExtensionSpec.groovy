6a51703a1e83352090672d4e639b52660113064b
Schalk W. Cronjé <ysb33r@gmail.com>
2019-04-22 18:00:13 +0200

    Move PDF plugin into its own subproject (#356)

--- asciidoctor-gradle-jvm-pdf/src/test/groovy/org/asciidoctor/gradle/jvm/pdf/AsciidoctorPdfThemeExtensionSpec.groovy
+++ asciidoctor-gradle-jvm-pdf/src/test/groovy/org/asciidoctor/gradle/jvm/pdf/AsciidoctorPdfThemeExtensionSpec.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.asciidoctor.gradle.jvm
+package org.asciidoctor.gradle.jvm.pdf
 
 import org.gradle.api.Project
 import org.gradle.api.UnknownDomainObjectException
