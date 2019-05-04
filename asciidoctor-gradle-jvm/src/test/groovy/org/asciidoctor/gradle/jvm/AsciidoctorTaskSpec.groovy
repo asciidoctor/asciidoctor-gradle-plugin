8731a66f0d2dd3e786b2627750d2389121b599e8
Schalk Cronje <ysb33r@gmail.com>
2019-05-04 21:06:53 +0200

    Add test for asciidoctor tasks with non-default names

--- asciidoctor-gradle-jvm/src/test/groovy/org/asciidoctor/gradle/jvm/AsciidoctorTaskSpec.groovy
+++ asciidoctor-gradle-jvm/src/test/groovy/org/asciidoctor/gradle/jvm/AsciidoctorTaskSpec.groovy
@@ -17,12 +17,8 @@ package org.asciidoctor.gradle.jvm
 
 import org.gradle.api.GradleException
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration
 import org.gradle.testfixtures.ProjectBuilder
-import spock.lang.Issue
 import spock.lang.Specification
-
-import java.util.concurrent.Callable
 
 /**
  * Asciidoctor task specification
@@ -229,8 +225,8 @@ class AsciidoctorTaskSpec extends Specification {
         when:
         asciidoctorTask {
             options = [
-                doctype   : 'book',
-                attributes: 'toc=right source-highlighter=coderay toc-title=Table\\ of\\ Contents'
+                    doctype   : 'book',
+                    attributes: 'toc=right source-highlighter=coderay toc-title=Table\\ of\\ Contents'
             ]
         }
 
@@ -242,9 +238,9 @@ class AsciidoctorTaskSpec extends Specification {
         when:
         asciidoctorTask {
             options = [doctype: 'book', attributes: [
-                'toc=right',
-                'source-highlighter=coderay',
-                'toc-title=Table of Contents'
+                    'toc=right',
+                    'source-highlighter=coderay',
+                    'toc-title=Table of Contents'
             ]]
         }
 
@@ -433,25 +429,13 @@ class AsciidoctorTaskSpec extends Specification {
         task.attributeProviders != project.extensions.getByType(AsciidoctorJExtension).attributeProviders
     }
 
-    @Issue('https://github.com/asciidoctor/asciidoctor-gradle-plugin/issues/405')
-    void 'Configuration that be set as a provider'() {
+
+    void 'Asciidoctor task with non-default name has different source directory'() {
         when:
-        project.allprojects {
-            configurations {
-                asciidoctorExample
-            }
-        }
-
-        AsciidoctorTask task = asciidoctorTask {
-            configurations = [project.provider({
-                project.configurations.asciidoctorExample
-            } as Callable<Configuration>)]
-        }
-
-        task.configurations
+        AsciidoctorTask task = project.tasks.create(name: 'kilowatt', type: AsciidoctorTask)
 
         then:
-        noExceptionThrown()
+        task.sourceDir == project.file('src/docs/asciidocKiloWatt')
     }
 
     AsciidoctorTask asciidoctorTask(Closure cfg) {
