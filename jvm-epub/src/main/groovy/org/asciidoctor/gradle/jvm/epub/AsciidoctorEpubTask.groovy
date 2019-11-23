000dcfe81e60e291343db249d808b05608331b81
Schalk Cronje <ysb33r@gmail.com>
2019-11-23 12:53:36 +0100

    Forward port PR #449 (@lptr)

--- jvm-epub/src/main/groovy/org/asciidoctor/gradle/jvm/epub/AsciidoctorEpubTask.groovy
+++ jvm-epub/src/main/groovy/org/asciidoctor/gradle/jvm/epub/AsciidoctorEpubTask.groovy
@@ -22,6 +22,7 @@ import org.asciidoctor.gradle.jvm.AbstractAsciidoctorTask
 import org.asciidoctor.gradle.base.AsciidoctorExecutionException
 import org.asciidoctor.gradle.base.process.ProcessMode
 import org.asciidoctor.gradle.kindlegen.KindleGenExtension
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.util.PatternSet
 import org.gradle.process.JavaForkOptions
@@ -37,6 +38,7 @@ import javax.inject.Inject
  * @author Schalk W. Cronjé
  */
 @CompileStatic
+@CacheableTask
 class AsciidoctorEpubTask extends AbstractAsciidoctorTask {
 
     public static final String KF8 = 'kf8'
